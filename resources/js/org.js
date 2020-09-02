26e255242c1a1e25a59d52b2de87a4a6356b1457
TEC <tec@tecosaur.com>
2020-09-02 12:27:07 +0800

    View source org in the same page using an iframe

--- resources/js/org.js
+++ resources/js/org.js
@@ -1,6 +1,32 @@
 function goToByScroll(id){
     $('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');
 }
-function show_org_source() {
-    window.location.assign(window.location.href.replace(/\/$/, "/index.html").replace(/\.html$/, ".org.html") )
+
+let src_page = undefined;
+async function fetch_src() {
+    const response = await fetch(window.location.href.replace(/\/$/, "/index.html").replace(/\.html$/, ".org.html"));
+    const data = await response.text();
+    src_page = data;
+}
+window.addEventListener('load', (event) => { fetch_src(); });
+
+async function show_org_source() {
+    const elem = document.getElementById("src")
+    if (elem) {
+        elem.parentNode.removeChild(elem);
+        document.body.style.overflowY = 'initial';
+    } else {
+        if (src_page === undefined) {
+            await fetch_src();
+        }
+        src = document.createElement("iframe");
+        src.setAttribute("id", "src");
+        src.src = "about:blank";
+        document.body.appendChild(src);
+        document.body.style.overflowY = 'hidden';
+        sDoc = src.contentWindow.document;
+        sDoc.open('text/html', 'replace');
+        sDoc.write(src_page);
+        sDoc.close();
+    }
 }
