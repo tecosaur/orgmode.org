6f861ad530ae14f553e1c8993211223603b6f982
Bastien <bzg@gnu.org>
2021-09-25 10:11:05 +0200

    Build and upload using SourceHut
    This adds the build manifest (.build.yml) and publish.sh script.
    HTML pages are uploaded to a dedicated server.

---
+++ publish.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+":"; exec emacs --quick --script "$0" -- "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+
+(require 'ox-html)
+(load "/usr/share/emacs/site-lisp/htmlize.el")
+
+(setq make-backup-files nil
+      debug-on-error t)
+
+(setq org-confirm-babel-evaluate nil
+      org-html-style-default ""
+      org-html-scripts ""
+      org-html-htmlize-output-type 'css
+      org-html-doctype "html5"
+      org-html-html5-fancy t
+      org-html-validation-link nil
+      org-html-postamble t
+      org-html-postamble-format
+       `(("en" ,(with-temp-buffer
+		  (insert-file-contents-literally "./resources/postamble.html")
+		  (buffer-substring-no-properties (point-min) (point-max))))))
+
+(dolist (org-file (directory-files-recursively default-directory "\\.org$"))
+  (let ((html-file (concat (file-name-directory org-file)
+			   (file-name-base org-file) ".html")))
+    (if (and (file-exists-p html-file)
+             (file-newer-than-file-p html-file org-file))
+	(message " [skipping] unchanged %s" org-file)
+      (message "[exporting] %s" (file-relative-name org-file default-directory))
+      (with-current-buffer (find-file org-file)
+	(condition-case err
+            (org-html-export-to-html)
+          (error (message (error-message-string err))))))))
